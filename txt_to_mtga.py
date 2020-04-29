8da7a7c3daf7c5208a61e1330813d075c46ec3cc
Jonn Mostovoy <jm@serokell.io>
2020-04-29 03:58:33 +0300

    Problem: forgot to add files
    Solution: add the files. Probably should have squashed and foce-pushed, but...

---
+++ txt_to_mtga.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from historic import all_cards_priv, all_sets_priv, historic_cards, txt_deck_to_mtga
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+app = Flask(__name__)
+
+cards = all_cards_priv()
+sets  = all_sets_priv()
+
+historicCards = historic_cards(cards, sets)
+
+@app.route('/')
+def query():
+   return render_template('txt_deck_to_mtga.html')
+
+@app.route('/converted', methods = ['POST'])
+def response():
+  pp.pprint(request.__dict__)
+  mtga_deck = txt_deck_to_mtga(request.form['deck'], historicCards)
+  return render_template('converted.html', result={'mtga_deck': mtga_deck})
+
+if __name__ == '__main__':
+	app.run(debug = True)
